94e54c1b86203b042cc7d13843516431c9536cdb
최한성 <hansung0904@naver.com>
2022-08-30 16:30:45 +0900

    Repository_Cleaning

--- Algorithm_Code_Refactor/Code_Refactor/src/com/algorithmrefactor/Number2557.java
+++
@@ -1,8 +0,0 @@
-package com.algorithmrefactor;
-
-/*Hello World!를 출력하세요!!*/
-public class Number2557 {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
