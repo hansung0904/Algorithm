81f2b019c8930e7812087a30da3a4e25865cba4c
최한성 <hansung0904@naver.com>
2022-09-02 10:33:50 +0900

    2022 09 02 String_Question_Number2908

--- src/stringquestion/Number2908.java
+++ src/stringquestion/Number2908.java
@@ -5,13 +5,16 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Number1152 {
+public class Number2908 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-        System.out.println(st.countTokens());
+        int inputNumberOne = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString());
+        int inputNumberTwo = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString());
 
+        System.out.println(inputNumberOne > inputNumberTwo ? inputNumberOne : inputNumberTwo);
+        br.close();
 
 
     }
