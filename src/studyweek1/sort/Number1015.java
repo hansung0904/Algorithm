5e664a0b47243639ca71efb170477d95e64775f1
hansung0904 <hansung0904@naver.com>
2023-01-09 19:18:27 +0900

    update : Number1015 comment

--- src/studyweek1/sort/Number1015.java
+++ src/studyweek1/sort/Number1015.java
@@ -11,24 +11,23 @@ public class Number1015 {
 
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(bufferedReader.readLine());
-        int[][] array = new int[n][3];
+        int[][] array = new int[n][3]; // Column 3, br로 입력받은 값을 Row 로 2차원 배열 선언`
 
-        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine()); // 문자열 쪼개기 
 
         for (int i = 0; i < n; i++) {
-            int val = Integer.parseInt(stringTokenizer.nextToken());
+            int val = Integer.parseInt(stringTokenizer.nextToken()); // String 타입이기 때문에 Integer 로 파싱
             array[i] = new int[]{val, i, 0};
         }
-        Arrays.sort(array, Comparator.comparingInt(o -> o[0]));
+        Arrays.sort(array, Comparator.comparingInt(o -> o[0])); // 0번째 컬럼 비교하면서 정렬
 
         for (int i = 0; i < n; i++) {
-            array[i][2] = i;
+            array[i][2] = i; // array[n][2] 에 0 ,1, 2 삽입
         }
-
-        Arrays.sort(array, Comparator.comparingInt(o -> o[1]));
+        Arrays.sort(array, Comparator.comparingInt(o -> o[1])); //1번째 컬럼 비교하면서 정렬
 
         for (int i = 0; i < n; i++) {
-            System.out.print(array[i][2] + " ");
+            System.out.print(array[i][2] + " "); // [0][2] ... [1][2],,, [2][2] 순으로 출력
         }
     }
 }
