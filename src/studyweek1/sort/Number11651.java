23229d2883aee2b0453e4eb38c7f6f1c7bf163f6
hansung0904 <hansung0904@naver.com>
2023-01-09 20:44:51 +0900

    update : AlgorithmStudy Week1

---
+++ src/studyweek1/sort/Number11651.java
@@ -0,0 +1,39 @@
+package studyweek1.sort;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Number11651 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int[][] array = new int[n][2];
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            array[i] = new int[]{y, x};
+
+        }
+
+        Arrays.sort(array, (e1, e2) -> {
+            if (e1[0] == e2[0]) {
+                return e1[1] - e2[1];
+            } else {
+                return e1[0] - e2[0];
+            }
+        });
+
+        for (int i = 0; i < n; i++) {
+            System.out.println(array[i][1] + " " + array[i][0]);
+        }
+    }
+}
